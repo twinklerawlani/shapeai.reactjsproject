1fcd4c9e2d536c59f802b4001dbe11b8d15a016e
twinklerawlani <86613434+twinklerawlani@users.noreply.github.com>
2021-06-28 20:10:41 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>javascript and react.js</h1>
+      <p>basic web dev react js bootcamp.it was a 7 day bootcamp.which was very helpful </p>
+    </div>
+  );
+}
+export default Info;
